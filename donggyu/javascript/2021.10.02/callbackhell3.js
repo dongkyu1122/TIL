df6eb18fe9eed3f5589da48a9e34e6769ea07d64
donggyu1122 <hanul6104@naver.com>
2021-10-06 10:45:12 +0900

    10.06
    추가

--- donggyu/javascript/2021.10.02/callbackhell3.js
+++ donggyu/javascript/2021.10.02/callbackhell3.js
@@ -1,22 +1,23 @@
 class UserStorge {
   loginUser(id, password, onSuccess, onError) {
     setTimeout(() => {
-      if(id==='ellie'&&password==='dream'||id==='coder'&&password==='academy') {
+      if(id === 'ellie' && password === 'dream' || id === 'coder' && password === 'academy') {
         onSuccess(id)
       } else {
         onError(new Error('not found'))
       }
     }, 2000)
   }
+
   getRoles(user, onSuccess, onError) {
     setTimeout(() => {
-      if(user==='ellie') {
-        onSuccess({name:user, role:'admin'})
+      if(user === 'ellie') {
+        onSuccess({name: user, role: 'admin'})
       } else {
         onError(new Error('no access'))
       }
-    },ㅏ1000)
-  }     
+    }, 1000)
+  }
 }
 
 const userStorge = new UserStorge
